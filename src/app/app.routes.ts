a87b300c8aa900a3814c3b57eb0e91e579eff166
jorgenano8 <jorgenano8@gmail.com>
2024-03-15 13:43:58 +0100

    Añadir layout de auth y sus componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,27 +4,46 @@ import { NuevoJugadorComponent } from './features/jugador/routes/nuevo-jugador/n
 import { DetalleJugadorComponent } from './features/jugador/routes/detalle-jugador/detalle-jugador.component';
 import { ActualizarJugadorComponent } from './features/jugador/routes/actualizar-jugador/actualizar-jugador.component';
 import { DashboardComponent } from './features/dashboard/routes/dashboard.component';
+import { SigninComponent } from './core/auth/routes/signin/signin.component';
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
+import { GeneralComponent } from './core/layouts/general/general.component';
+import { SignupComponent } from './core/auth/routes/signup/signup.component';
 
 export const routes: Routes = [
   {
+    path: 'signin',
+    component: AuthLayoutComponent,
+    children: [{ path: '', component: SigninComponent }],
+  },
+  {
+    path: 'signup',
+    component: AuthLayoutComponent,
+    children: [{ path: '', component: SignupComponent }],
+  },
+  {
     path: 'dashboard',
-    component: DashboardComponent,
+    component: GeneralComponent,
+    children: [{ path: '', component: DashboardComponent }],
   },
   {
     path: 'jugadores',
-    component: ListaJugadorComponent,
+    component: GeneralComponent,
+    children: [{ path: '', component: ListaJugadorComponent }],
   },
   {
     path: 'jugadores/nuevo',
-    component: NuevoJugadorComponent,
+    component: GeneralComponent,
+    children: [{ path: '', component: NuevoJugadorComponent }],
   },
   {
     path: 'jugadores/:id',
-    component: DetalleJugadorComponent,
+    component: GeneralComponent,
+    children: [{ path: '', component: DetalleJugadorComponent }],
   },
   {
     path: 'jugadores/:id/actualizar',
-    component: ActualizarJugadorComponent,
+    component: GeneralComponent,
+    children: [{ path: '', component: ActualizarJugadorComponent }],
   },
-  { path: '**', redirectTo: 'dashboard' },
+  { path: '**', redirectTo: 'signin' },
 ];

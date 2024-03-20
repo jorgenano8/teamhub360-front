d88ee04fe85e40528705b28214060e23e617829a
jorgenano8 <jorgenano8@gmail.com>
2024-03-20 18:10:49 +0100

    env links

--- src/app/features/jugador/services/jugador.service.ts
+++ src/app/features/jugador/services/jugador.service.ts
@@ -2,33 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Jugador } from '../models/jugador.model';
+import { environment } from '../../../../environments/environment';
+
+const jugadorURL: string = environment.url_api + 'jugador';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JugadorService {
+  constructor(private http: HttpClient) {}
 
-  jugadorURL:string="http://localhost:8080/jugador"
-
-  constructor(private http: HttpClient) { }
-
-  findAll(){
-    return this.http.get<Jugador[]>(this.jugadorURL);
+  findAll() {
+    return this.http.get<Jugador[]>(jugadorURL);
   }
 
-  findById(jugadorID: any){
-    return this.http.get<Jugador>(this.jugadorURL + "/" + jugadorID);
+  findById(jugadorID: any) {
+    return this.http.get<Jugador>(jugadorURL + '/' + jugadorID);
   }
 
-  save(jugador: Jugador){
-    return this.http.post<Jugador>(this.jugadorURL, jugador);
+  save(jugador: Jugador) {
+    return this.http.post<Jugador>(jugadorURL, jugador);
   }
 
-  update(jugadorID: any, jugador: Jugador){
-    return this.http.put<Jugador>(this.jugadorURL + "/" + jugadorID, jugador);
+  update(jugadorID: any, jugador: Jugador) {
+    return this.http.put<Jugador>(jugadorURL + '/' + jugadorID, jugador);
   }
 
-  delete(jugadorID: any){
-    return this.http.delete(this.jugadorURL + "/" + jugadorID);
+  delete(jugadorID: any) {
+    return this.http.delete(jugadorURL + '/' + jugadorID);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   url_api: 'http://localhost:8080/',
+  url_api_auth: 'http://localhost:8081/auth/',
 };
